660aaad8f2f2744d4f103347fba5c41d94035476
Taylor <taylor.hackbart@du.edu>
2020-11-19 15:11:29 -0800

    added topology to mongo constuctor

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget"),
 { useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false}
+  useFindAndModify: false,
+  useUnifiedTopology: true }
 
 // routes
 app.use(require("./routes/api.js"));
