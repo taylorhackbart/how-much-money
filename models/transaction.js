5bc5d49cc41843edc4d470fb881cabfa6944e156
Taylor <taylor.hackbart@du.edu>
2020-11-19 08:43:01 -0800

    heroku

--- models/transaction.js
+++ models/transaction.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
+const transactionSchema = new Schema( 
   {
     name: {
       type: String,
